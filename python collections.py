f0e0a91f1af277dc4b4f442e26e87daab6f5de30
tabishhameed <tabishhameed34@gmail.com>
2021-10-23 04:02:31 +0800

    update default dict

--- python collections.py
+++ python collections.py
@@ -22,9 +22,21 @@ alma_maters = defaultdict(list)
 for coworker, place in coworkers:
     alma_maters [coworker].append(place)
 
-alma_maters.default_factory = None
+#alma_maters.default_factory = None
 
 print(alma_maters['Rolf'])
 print(alma_maters['Dam'])
 print(alma_maters['Fam'])
 
+
+
+my_company = 'tabish'
+
+coworkers = ['jen', 'sam', 'fam', 'ham']
+other_coworkers = [('Rolf', 'Apple'), ('Tam', 'google')]
+coworker_compaines = defaultdict(lambda : my_company)
+
+for person, company in other_coworkers:
+    coworker_compaines [person] = company
+print(coworker_compaines [coworker[0]])
+print(coworker_compaines['Rolf'])
