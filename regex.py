a97fc899eb7fe7ca4f5f0317f1118a6c8b9de2ab
tabishhameed <tabishhameed34@gmail.com>
2021-11-29 18:57:25 +0800

    regualar expression

--- regex.py
+++ regex.py
@@ -11,3 +11,11 @@ print(matches.group(1))
 price_without_comma = matches.group(1).replace(',', '')
 price_number = float(price_without_comma)
 print(price_number)
+
+
+#def is_filename_safe(filename):
+ #  regex = '^[a-zA-Z0-9][a-zA-Z0-9_()-]*(\.jpg|\.jpeg|\.png|\.gif)$'
+  #  return re.match(regex, filename) is not None
+
+
+
